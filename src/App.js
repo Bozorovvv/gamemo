d96e72d7b49a6af7d72441646762ad7a8e94b2c1
farhod <farhod.gmail.com>
2021-10-24 11:11:28 +0500

    memogame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { shuffle } from 'lodash'
+import Images from './Images'
 
 function App() {
+  const [cards, setCards] = useState(shuffle([...Images, ...Images]))
+  const [clicks, setClicks] = useState(0)
+  const [won, setWon] = useState(false)
+  const [activeCards, setActiveCards] = useState([])
+  const [foundPairs, setFoundPairs] = useState([])
+  const [seconds, setSeconds] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+
+  function toggle() {
+    setIsActive(!isActive)
+  }
+  function reset() {
+    setSeconds(0)
+    setIsActive(false)
+    setActiveCards([])
+    setFoundPairs([])
+    setWon(false)
+    setClicks(0)
+  }
+  useEffect(() => {
+    let interval = null
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1)
+      }, 1000)
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+  }, [isActive, seconds])
+
+  function flipCard(index) {
+    if (won) {
+      setCards(shuffle([...Images, ...Images]))
+      setFoundPairs([])
+      setWon(false)
+      setClicks(0)
+      setSeconds(0)
+      setIsActive(false)
+    }
+    if (activeCards.length === 0) {
+      setActiveCards([index])
+    }
+    if (activeCards.length === 1) {
+      const firstIndex = activeCards[0]
+      const secondIndex = index
+      if (cards[firstIndex] === cards[secondIndex]) {
+        if (foundPairs.length + 2 === cards.length) {
+          setWon(true)
+        }
+        setFoundPairs([...foundPairs, firstIndex, secondIndex])
+      }
+      setActiveCards([...activeCards, index])
+    }
+    if (activeCards.length === 2) {
+      setActiveCards([index])
+    }
+    setClicks(clicks + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="board">
+        {cards.map((card, index) => {
+          const flippedToFront =
+            activeCards.indexOf(index) !== -1 ||
+            foundPairs.indexOf(index) !== -1
+          return (
+            <div
+              className={'card-outer ' + (flippedToFront ? 'flipped' : '')}
+              onClick={() => flipCard(index)}
+            >
+              <div className="card">
+                <div className="front">
+                  <img src={card} alt="" />
+                </div>
+                <div className="back" />
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      <div>
+        <div className="timer">
+          <h1>GAMEMO</h1>
+          <div>
+            <button className="start" onClick={toggle}>
+              start
+            </button>
+            <button className="stop" onClick={reset}>
+              stop
+            </button>
+            <div className="time">Your time: {seconds}</div>
+          </div>
+        </div>
+        <div className="stats">
+          {won && (
+            <>
+              You won the game! Congratulations!
+              <br />
+            </>
+          )}
+          Clicks: {clicks} &nbsp;&nbsp;&nbsp; Found pairs:{' '}
+          {foundPairs.length / 2}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Images.js
@@ -0,0 +1,39 @@
+import img1 from './images/camel.png'
+import img2 from './images/cobra.png'
+import img3 from './images/crocodile.png'
+import img4 from './images/deer.png'
+import img5 from './images/dog.png'
+import img6 from './images/eagle.png'
+import img7 from './images/elephant.png'
+import img8 from './images/fox.png'
+import img9 from './images/horse.png'
+import img10 from './images/leopard.png'
+import img11 from './images/lion.png'
+import img12 from './images/monkey.png'
+import img13 from './images/owl.png'
+import img14 from './images/parrot.png'
+import img15 from './images/pigeon.png'
+import img16 from './images/rabbit.png'
+import img17 from './images/tiger.png'
+import img18 from './images/wolf.png'
+
+export default [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+];
